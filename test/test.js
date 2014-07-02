0c5ca300cffa8be2577b2e5d927b75489d119bb9
Adam Bozanich <adam.boz@gmail.com>
2014-07-01 17:30:20 -0700

    Immediately load configuration

--- test/test.js
+++ test/test.js
@@ -1,53 +1,53 @@
 var assert = require("assert"),
-    config = require("../lib/config"),
+    loader = require("../lib/loader"),
     path   = require("path"),
     _      = require("underscore");
 
-suite("Config",function(){
+suite("Loader",function(){
   suite("constructor arguments",function(){
     test("basedir should default to the current directory",function(){
-      assert.equal(path.join(process.cwd(),'config'),new config().basedir);
+      assert.equal(path.join(process.cwd(),'config'),new loader().basedir);
     });
     test("basedir should be overridden by the first argument",function(){
-      assert.equal("abc",new config("abc").basedir);
-      assert.equal("abc",new config("abc").basedir);
+      assert.equal("abc",new loader("abc").basedir);
+      assert.equal("abc",new loader("abc").basedir);
     });
   });
   suite("utilities",function(){
     setup(function(){
-      this.config = new config("bar");
+      this.loader = new loader("bar");
     });
     test("#stripext",function(){
-      assert.equal("foo",this.config.stripext("foo.json","json"));
+      assert.equal("foo",this.loader.stripext("foo.json","json"));
     });
     suite("#fileInfo",function(){
       test("it constructs a valid object",function(){
-        var fi = this.config.fileInfo("foo.json");
+        var fi = this.loader.fileInfo("foo.json");
         assert.equal("json",fi.type);
         assert.equal("foo" ,fi.name);
         assert.equal("bar/foo.json" ,fi.path);
       });
       test("it returns null on an invalid filename",function(){
-        assert.equal(null,this.config.fileInfo("test"));
-        assert.equal(null,this.config.fileInfo("test."));
-        assert.equal(null,this.config.fileInfo(".json"));
+        assert.equal(null,this.loader.fileInfo("test"));
+        assert.equal(null,this.loader.fileInfo("test."));
+        assert.equal(null,this.loader.fileInfo(".json"));
       });
     });
-    suite("#mergeConfig",function(){
+    suite("#mergeLoader",function(){
       test("it assigns a new key when empty", function(){
         var current = {};
-        current = this.config.mergeConfig(current,'foo',{"a":1});
+        current = this.loader.mergeLoader(current,'foo',{"a":1});
         assert.equal(current.foo.a,1);
       });
       test("it assigns a new key and retains the old keys",function(){
         var current = {"foo": {"a": 1}};
-        current = this.config.mergeConfig(current,'bar',{"b":2});
+        current = this.loader.mergeLoader(current,'bar',{"b":2});
         assert.equal(current.foo.a,1);
         assert.equal(current.bar.b,2);
       });
       test("it does not perform a true deep-merge",function(){
         var current = {"foo": {"a": 1}};
-        current = this.config.mergeConfig(current,'foo',{"b":2});
+        current = this.loader.mergeLoader(current,'foo',{"b":2});
         assert.equal(current.foo.a,undefined);
         assert.equal(current.foo.b,2);
       });
@@ -56,20 +56,20 @@ suite("Config",function(){
   suite("file methods",function(){
     setup(function(){
       this.basedir = path.join(__dirname,"config");
-      this.config  = new config(this.basedir);
+      this.loader  = new loader(this.basedir);
     });
     test("#files",function(){
-      var files = this.config.files();
+      var files = this.loader.files();
       assert.equal(files.length,3);
       assert.ok(_.findWhere(files,{"type":"json"}));
       assert.ok(_.findWhere(files,{"type":"yml"}));
     });
     test("#load",function(){
-      var cfg = this.config.load();
+      var cfg = this.loader.load();
       assert.deepEqual(cfg,{"foo":{"a":1,"b":2}});
     });
-    test("Config.load",function(){
-      var cfg = config.load(this.basedir);
+    test("Loader.load",function(){
+      var cfg = loader.load(this.basedir);
       assert.deepEqual(cfg,{"foo":{"a":1,"b":2}});
     });
   });
